c5f785891cdee02c8d84e42dd98fd1ddf9dc647f
COOKIE <dominiknageler343@gmail.com>
2026-05-21 13:47:53 +0200

    database added

---
+++ src/database.py
@@ -0,0 +1,17 @@
+from  sqlalchemy import create_engine
+from  sqlalchemy.orm import sessionmaker, declarative_base
+SQL_ALCHEMY_DATABASE_URL = "sqlite:///Statify.db"
+
+engine = create_engine(SQL_ALCHEMY_DATABASE_URL,connect_args={"check_same_thread": False})
+
+
+Sesionlocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+Base = declarative_base()
+def get_db():
+    db = Sesionlocal()
+
+    try:
+        yield db
+    finally:
+      db.close()
