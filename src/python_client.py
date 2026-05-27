b2f58660f5b866bcdc9496f8eac214d212f131b3
YoshyRias <jnsmarte@gmail.com>
2026-05-27 22:12:02 +0200

    outsorced secrets to .env file

--- src/python_client.py
+++ src/python_client.py
@@ -1,12 +1,14 @@
-from spotipy.oauth2 import SpotifyOAuth
 import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-# TODO: client_id and client_secret a .env file (.env file also in gitignore)
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    client_id="1549b84275a94fb2892effbc257c06a3",
-    client_secret="643062d2510446969d16dea9b9c53eba",
-    redirect_uri="http://127.0.0.1:8888/callback",
+    client_id=os.getenv("SPOTIFY_CLIENT_ID"),
+    client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
+    redirect_uri=os.getenv("SPOTIFY_REDIRECT_URI"),
     scope="user-read-recently-played user-top-read playlist-read-private"
 ))
 
