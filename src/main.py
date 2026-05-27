dcb3632a354c7148d3199f8fe0773adcfee673e8
YoshyRias <jnsmarte@gmail.com>
2026-05-27 15:25:00 +0200

    spotipy implementation

--- src/main.py
+++ src/main.py
@@ -3,17 +3,28 @@ from fastapi import FastAPI,Request
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from starlette import status
-
 from routers import artitst,genre,playlist,stats,track,track_Record,user
 from database import engine
 import models
+from spotipy.oauth2 import SpotifyOAuth
+import spotipy
 
+# TODO: client_id and client_secret a .env file (.env file also in gitignore)
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id="1549b84275a94fb2892effbc257c06a3",
+    client_secret="643062d2510446969d16dea9b9c53eba",
+    redirect_uri="http://127.0.0.1:8888/callback",
+    scope="user-read-recently-played user-top-read playlist-read-private"
+))
 
+# 
+sp.current_user()
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title='Statify', description='Statfy API', version='1.0.0')
 app.include_router(user.router)
+
 
 
 @app.exception_handler(RequestValidationError)
@@ -30,8 +41,9 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 
 @app.get("/")
 def root():
-    return {"Message": "Welcome to Statify go to /docs for the API"}
+    return sp.current_user_recently_played(limit=10)
+    #return {"Message": "Welcome to Statify go to /docs for the API"}
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8888)
